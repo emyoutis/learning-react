cb2243ad0d5d29adfa1a78ea532271d084afc51f
EmiTis <emitis89@gmail.com>
2020-04-17 15:53:10 +0430

    rearange task form

--- src/components/todo-panel/TaskForm.js
+++ src/components/todo-panel/TaskForm.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import TaskInput from "./TaskInput";
 import './task-form.css'
-import TaskButton from "./TaskButton";
 
 function TaskForm() {
     return (
         <form className="task-form">
             <div>
                 <h2>Add Task:</h2>
-                <TaskInput/>
-                <TaskButton/>
+                <div className="field">
+                    <input
+                        type="text"
+                        placeholder="Task Title"
+                    />
+                </div>
+                <div className="btn">
+                    <button type="submit">Save</button>
+                </div>
             </div>
         </form>
     );
